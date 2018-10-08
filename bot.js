ad29f4334daaaa4de03f22d31fd148dc00b6e354
Cannonski13 <43939704+Cannonski13@users.noreply.github.com>
2018-10-08 13:44:51 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const prefix = "?" 
-
-client.on("ready", () => {
-client.user.setGame('?ping')
-console.log("this bot has booted up Ready to function")
-});
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === '?ping') {
-    	message.reply('pong');
-});
-
-client.on('message', message => {
-    if (message.content === '?help') {
-    	message.reply('https://discord.gg/nBCrBQZ');
+    if (message.content === '?updates') {
+    	message.reply('Updates is here: https://docs.google.com/document/d/1N9rm2l-dF305MAUNLJp80HhokmWhZ-1dQ2xbm-CMv_s/edit?usp=sharing')  
+                     
+ 
   	}
 });
+client.on('message', message => {
+    if (message.content === '?help') {
+    	message.reply('Join the support server for help: https://discord.gg/nBCrBQZ') 
+  	}
+});
+client.on('message', message => {
+    if (message.content === '?ping') {
+    	message.reply('Pong');
+  	}
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
