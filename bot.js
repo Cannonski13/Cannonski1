4e7816dfd7abdc7626be6cb91e20b5fed493e786
Kaznith <43898226+Kaznith@users.noreply.github.com>
2018-10-12 16:25:36 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,35 +2,50 @@ const Eris = require('eris');
 const bot = new Eris(process.env.BOT_TOKEN);
 
 bot.on('ready', () => {
-	console.log('Connected');
-	bot.editStatus('online', {name: 'Command ?', type :0})
+    console.log('Connected');
+    bot.editStatus('online', {name: 'Command ?', type :0})
 });
 
 bot.on('messageCreate', msg => {
-	const usedby = `Command ${msg.content} used by ${msg.member.username}#${msg.member.discriminator} (${msg.member.id})`;
+    const usedby = `Command ${msg.content} used by ${msg.member.username}#${msg.member.discriminator} (${msg.member.id})`;
 
-	if (msg.author.bot) return;
+    if (msg.author.bot) return;
 
-	
-	if (msg.content === '?help') {
-		bot.createMessage(msg.channel.id, 'If you are needing help join the support server here: https://discord.gg/nBCrBQZ')
-		.catch((e) => {console.log(e)});
+    if (msg.content === '!updates') {
+        bot.createMessage(msg.channel.id, {embed: {
+            description: 'Our list of updates can be found in the Official Server (Run command !help to join)'
+        }})
+        .catch((e) => {console.log(e)});
 
-		console.log(usedby);
-	};
-	  
-    if (msg.content === '?addme') {
-		bot.createMessage(msg.channel.id, 'You can add me here: https://discordapp.com/api/oauth2/authorize?client_id=498631058690932736&permissions=0&scope=bot')
-		.catch((e) => {console.log(e)});
+        console.log(usedby);
+    };
+        
+    if (msg.content === '!commands') {
+        bot.createMessage(msg.channel.id, {embed: {
+            description: 'Commands can be found here: https://docs.google.com/document/d/1N9rm2l-dF305MAUNLJp80HhokmWhZ-1dQ2xbm-CMv_s/edit?usp=sharing'
+        }})
+        .catch((e) => {console.log(e)});
 
-		console.log(usedby);
-  	};
-    if (msg.content === '?ping') {
-		bot.createMessage(msg.channel.id, 'Pong')
-		.catch((e) => {console.log(e)});
+        console.log(usedby);
+    };
 
-		console.log(usedby);
-	};
+    if (msg.content === '!help') {
+        bot.createMessage(msg.channel.id, {embed: {
+            description: 'If you require help than please join the support server here: https://discord.gg/zw8Yzhf'
+        }})
+        .catch((e) => {console.log(e)});
+
+        console.log(usedby);
+    };
+      
+    if (msg.content === '!addme') {
+        bot.createMessage(msg.channel.id, {embed: {
+            description: 'In progress'
+        }})
+        .catch((e) => {console.log(e)});
+
+        console.log(usedby);
+      };
 
 });
 
